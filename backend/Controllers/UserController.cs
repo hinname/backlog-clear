9778ed4f8be025a503232fea0d5300adebe68aff
Hinkami <herivelton.pro@gmail.com>
2024-10-17 17:20:07 -0300

    Testing response type for swagger

--- backend/Controllers/UserController.cs
+++ backend/Controllers/UserController.cs
@@ -6,9 +6,23 @@ namespace BacklogClear.Controllers;
 [ApiController]
 public class UserController : ControllerBase
 {
+    
+    private class User
+    {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+    }
+    
+    
     [HttpGet]
+    [ProducesResponseType(typeof(User),StatusCodes.Status200OK)]
     public IActionResult GetUser()
     {
-        return Ok("Hello World");
+        var response = new User()
+        {
+            Email = "Teste@tes.com",
+            Name = "Herivelton"
+        };
+        return Ok(response);
     }
 }
