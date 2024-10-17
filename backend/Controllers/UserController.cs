8f8440d9e764545dc4812aecf25dff7005e81fe0
Hinkami <herivelton.pro@gmail.com>
2024-10-17 17:37:51 -0300

    Testing route template

--- backend/Controllers/UserController.cs
+++ backend/Controllers/UserController.cs
@@ -9,20 +9,27 @@ public class UserController : ControllerBase
     
     private class User
     {
+        public int Id { get; set; }
         public string? Name { get; set; }
         public string? Email { get; set; }
     }
     
     
     [HttpGet]
+    [Route("{id}")]
     [ProducesResponseType(typeof(User),StatusCodes.Status200OK)]
-    public IActionResult GetUser()
+    [ProducesResponseType(typeof(string),StatusCodes.Status404NotFound)]
+    public IActionResult GetUser(int id)
     {
         var response = new User()
         {
+            Id = 1,
             Email = "Teste@tes.com",
             Name = "Herivelton"
         };
-        return Ok(response);
+        if (id == response.Id)
+            return Ok(response);
+        
+        return NotFound("User not found");
     }
 }
