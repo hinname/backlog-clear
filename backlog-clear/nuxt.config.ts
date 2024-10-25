483923ffea0e9f9cc1be1b482dd7850428a4ea4d
Hinkami <herivelton.pro@gmail.com>
2024-10-25 18:09:13 -0300

    add tailwind to nuxt app and index page created

--- backlog-clear/nuxt.config.ts
+++ backlog-clear/nuxt.config.ts
@@ -1,5 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  app: {
+    head: {
+      title: 'Backlog Clear'
+    }
+  },
+  $development: {
+    app: {
+      head: {
+        title: 'Dev' + ' | '
+      }
+    }
+  }
 })
