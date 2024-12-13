63352727da97a5d9a5c39a2114298ae694109a0f
hinname <herivelton.pro@gmail.com>
2024-12-13 18:33:07 -0300

    Report controller started

---
+++ backend/src/BacklogClear.Api/Controllers/ReportController.cs
@@ -0,0 +1,20 @@
+using System.Net.Mime;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BacklogClear.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+        [HttpGet("excel")]
+        [ProducesResponseType(typeof(FileContentResult),StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> GetExcel()
+        {
+            byte[] file = new byte[1];
+            return File(file, MediaTypeNames.Application.Octet, "report.xlsx");
+        }
+    }
+}
