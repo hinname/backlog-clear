6d79102e89665bd2c86d0b4906dcdf83850aa8e1
Hinkami <herivelton.pro@gmail.com>
2025-03-06 18:43:55 -0300

    reset frontend

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,13 +1,14 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ]
   },
   app: {
     head: {
---
+++ frontend/pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+useHead({
+  titleTemplate: '%s | Backlog Clear',
+  meta: [
+    {
+      name: 'description',
+      content: 'Backlog Clear'
+    }
+  ]
+})
+</script>
+
+<template>
+  <h1>Backlog Clear</h1>
+</template>
