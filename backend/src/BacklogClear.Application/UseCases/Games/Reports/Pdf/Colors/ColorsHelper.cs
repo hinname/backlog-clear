68c098151261ed37f6e15e9527654c9518100de2
hinname <herivelton.pro@gmail.com>
2025-04-04 17:59:17 -0300

    cores provisorias

--- backend/src/BacklogClear.Application/UseCases/Games/Reports/Pdf/Colors/ColorsHelper.cs
+++ backend/src/BacklogClear.Application/UseCases/Games/Reports/Pdf/Colors/ColorsHelper.cs
@@ -1,6 +1,13 @@
+using MigraDoc.DocumentObjectModel;
+
 namespace BacklogClear.Application.UseCases.Games.Register.Reports.Pdf.Colors;
 
 public static class ColorsHelper
 {
-    
+    public readonly static Color RED_DARK = Color.Parse("#DF340C");
+    public readonly static Color RED_LIGHT = Color.Parse("#F5C2B6");
+    public readonly static Color GREEN_DARK = Color.Parse("#D7E5E5");
+    public readonly static Color GREEN_LIGHT = Color.Parse("#F5F9F8");
+    public readonly static Color BLACK = Color.Parse("#000000");
+    public readonly static Color WHITE = Color.Parse("#FFFFFF");
 }
