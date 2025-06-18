229d2227c54a7f3e459df901e1291252dff09c02
Hinkami <herivelton.pro@gmail.com>
2025-06-18 14:03:39 -0300

    modify nuxtlink

--- frontend/components/Header.vue
+++ frontend/components/Header.vue
@@ -6,17 +6,17 @@ const route = useRoute();
 <template>
   <header class="flex items-center justify-between p-4">
     <h1 class="font-extrabold text-4xl text-emerald-500">
-      <nuxt-link to="/">Backlog Clear</nuxt-link>
+      <NuxtLink to="/">Backlog Clear</NuxtLink>
     </h1>
     <nav>
       <ul class="flex flex-wrap gap-6 mr-2">
         <li v-if="route.path == '/' || route.path == '/signup'">
-          <nuxt-link to="/login" class="font-bold text-emerald-500 py-2 px-4 border rounded-md border-emerald-500
-                   hover:bg-emerald-500 hover:text-black hover:duration-300">Login</nuxt-link>
+          <NuxtLink to="/login" class="font-bold text-emerald-500 py-2 px-4 border rounded-md border-emerald-500
+                   hover:bg-emerald-500 hover:text-black hover:duration-300">Login</NuxtLink>
         </li>
         <li v-if="route.path == '/' || route.path == '/login'">
-          <nuxt-link to="/signup" class="font-bold text-emerald-500 py-2 px-4 border rounded-md border-emerald-500
-                  hover:bg-emerald-500 hover:text-black hover:duration-300">Sign Up</nuxt-link>
+          <NuxtLink to="/signup" class="font-bold text-emerald-500 py-2 px-4 border rounded-md border-emerald-500
+                  hover:bg-emerald-500 hover:text-black hover:duration-300">Sign Up</NuxtLink>
         </li>
       </ul>
     </nav>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -19,8 +19,8 @@ useHead({
     <section class="flex flex-col items-center justify-center h-screen">
       <h2 class="text-5xl font-bold text-emerald-500">Welcome to Backlog Clear</h2>
       <p class="text-2xl text-gray-500">A simple way to manage the games you play</p>
-      <nuxt-link to="/signup" class="font-bold text-emerald-500 py-2 px-4 border rounded-md border-emerald-500 mt-2 text-xl
-                  hover:bg-emerald-500 hover:text-black hover:duration-300">SIGN UP NOW</nuxt-link>
+      <NuxtLink to="/signup" class="font-bold text-emerald-500 py-2 px-4 border rounded-md border-emerald-500 mt-2 text-xl
+                  hover:bg-emerald-500 hover:text-black hover:duration-300">SIGN UP NOW</NuxtLink>
     </section>
   </main>
 </template>
