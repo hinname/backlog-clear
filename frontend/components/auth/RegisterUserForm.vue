883bd2bb417bb0f8ab772ffa7acc92c0d5bd047c
Hinkami <herivelton.pro@gmail.com>
2025-06-18 13:53:03 -0300

    RegisterUserForm.vue formData

--- frontend/components/auth/RegisterUserForm.vue
+++ frontend/components/auth/RegisterUserForm.vue
@@ -1,14 +1,28 @@
 <script setup lang="ts">
+import {reactive} from "vue";
+import type {RegisterUserForm} from "@/types/app/RegisterUserForm";
+
+const formData = reactive<RegisterUserForm>({
+  username: '',
+  email: '',
+  password: '',
+  confirmPassword: ''
+});
+
+const handleSubmit = (event: Event): void => {
+  event.preventDefault();
+}
 
 </script>
 
 <template>
-  <form class="flex flex-col gap-4 mt-4 w-full">
+  <form @submit="handleSubmit" class="flex flex-col gap-4 mt-4 w-full">
     <div class="flex flex-col gap-2">
       <label for="username" class="font-bold text-emerald-500">Username</label>
       <input
           type="text" id="username" name="username"
           class="border border-emerald-500 rounded-md p-2"
+          v-model="formData.username"
       />
     </div>
     <div class="flex flex-col gap-2">
@@ -16,6 +30,7 @@
       <input
           type="email" id="email" name="email"
           class="border border-emerald-500 rounded-md p-2"
+          v-model="formData.email"
       />
     </div>
     <div class="flex flex-col gap-2">
@@ -23,19 +38,20 @@
       <input
           type="password" id="password" name="password"
           class="border border-emerald-500 rounded-md p-2"
+          v-model="formData.password"
       />
     </div>
     <div class="flex flex-col gap-2">
       <label for="confirmPassword" class="font-bold text-emerald-500">Confirm Password</label>
-      <input type="password" id="confirmPassword" name="confirmPassword" class="border border-emerald-500 rounded-md p-2" />
+      <input
+          type="password" id="confirmPassword" name="confirmPassword"
+          class="border border-emerald-500 rounded-md p-2"
+          v-model="formData.confirmPassword"
+      />
     </div>
     <button
         type="submit"
         class="font-bold bg-emerald-500 text-black py-2 px-4 border rounded-md border-emerald-500 mt-2
-                  hover:bg-emerald-700 hover:border-emerald-700 hover:duration-150">Sign Up</button>
+                  hover:bg-emerald-700 hover:border-emerald-700 hover:duration-150 cursor-pointer">Sign Up</button>
   </form>
 </template>
-
-<style scoped>
-
-</style>
---
+++ frontend/types/app/RegisterUserForm.ts
@@ -0,0 +1,6 @@
+export interface RegisterUserForm {
+    username: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+}
