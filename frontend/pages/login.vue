de4b7183ef1b82224ad8376f4b2d6e1eed56833a
Hinkami <herivelton.pro@gmail.com>
2025-06-18 13:55:16 -0300

    add LoginForm component

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -1,19 +1,5 @@
 <script setup lang="ts">
-import {ref} from "vue";
-
-const email = ref('');
-const password = ref('');
-
-const handleSubmit = (event: Event) => {
-  event.preventDefault();
-
-  console.log('Email:', email.value);
-  console.log('Password:', password.value);
-
-  email.value = '';
-  password.value = '';
-};
-
+import LoginForm from "~/components/auth/LoginForm.vue";
 </script>
 
 <template>
@@ -23,27 +9,7 @@ const handleSubmit = (event: Event) => {
       <fieldset class="w-full flex flex-col items-center">
         <legend class="text-4xl font-bold text-emerald-500 m-auto">Login</legend>
         <p class="text-2xl text-gray-500">Welcome back!</p>
-        <form class="flex flex-col gap-4 mt-4 w-full">
-          <div class="flex flex-col gap-2">
-            <label for="email" class="font-bold text-emerald-500">Email</label>
-            <input
-                type="email" id="email" name="email"
-                class="border border-emerald-500 rounded-md p-2"
-                v-model="email" />
-          </div>
-          <div class="flex flex-col gap-2">
-            <label for="password" class="font-bold text-emerald-500">Password</label>
-            <input
-                type="password" id="password" name="password"
-                class="border border-emerald-500 rounded-md p-2"
-                v-model="password" />
-          </div>
-          <button
-              type="submit"
-              class="font-bold bg-emerald-500 text-black py-2 px-4 border rounded-md border-emerald-500 mt-2
-                  hover:bg-emerald-700 hover:border-emerald-700 hover:duration-150"
-              @click="handleSubmit">Login</button>
-        </form>
+        <LoginForm />
       </fieldset>
     </section>
   </main>
