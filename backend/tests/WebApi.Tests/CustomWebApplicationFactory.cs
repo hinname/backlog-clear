7aee3e376278ed675a6e5575e1cdef75d8c963d6
Hinkami <herivelton.pro@gmail.com>
2025-06-30 10:34:46 -0300

    Add DoLogin Test

--- backend/tests/WebApi.Tests/CustomWebApplicationFactory.cs
+++ backend/tests/WebApi.Tests/CustomWebApplicationFactory.cs
@@ -1,4 +1,7 @@
+using BacklogClear.Domain.Entities;
+using BacklogClear.Domain.Security.Crytography;
 using BacklogClear.Infrastructure.DataAccess;
+using CommonTestUtilities.Entities;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
@@ -8,6 +11,18 @@ namespace WebApi.Tests;
 
 public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 {
+    private User _user;
+    private string _password;
+
+    public string GetEmail()
+    {
+       return _user.Email;
+    }
+
+    public string GetPassword()
+    {
+        return _password;
+    }
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.UseEnvironment("Test")
@@ -20,6 +35,21 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
                     config.UseInMemoryDatabase("InMemoryDatabase");
                     config.UseInternalServiceProvider(provider);
                 });
+
+                var scope = services.BuildServiceProvider().CreateScope();
+                var dbContext = scope.ServiceProvider.GetRequiredService<BacklogClearDbContext>();
+                var passwordEncrypter = scope.ServiceProvider.GetRequiredService<IPasswordEncrypter>();
+                
+                StartDatabase(dbContext, passwordEncrypter);
             });
+    }
+
+    private void StartDatabase(BacklogClearDbContext dbContext, IPasswordEncrypter passwordEncrypter)
+    {
+        _user = UserBuilder.Build();
+        _password = _user.Password;
+        _user.Password = passwordEncrypter.Encrypt(_user.Password);
+        dbContext.Users.Add(_user);
+        dbContext.SaveChanges();
     }
 }
---
+++ backend/tests/WebApi.Tests/Login/DoLogin/DoLoginTest.cs
@@ -0,0 +1,118 @@
+using System.Globalization;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json;
+using BacklogClear.Communication.Requests.Login;
+using BacklogClear.Exception.Resources;
+using FluentAssertions;
+using WebApi.Tests.InlineData;
+
+namespace WebApi.Tests.Login.DoLogin;
+
+public class DoLoginTest : IClassFixture<CustomWebApplicationFactory>
+{
+    private const string METHOD = "api/login";
+
+    private readonly string _email;
+    private readonly string _password;
+    private readonly HttpClient _httpClient;
+
+    public DoLoginTest(CustomWebApplicationFactory factory)
+    {
+        _httpClient = factory.CreateClient();
+        _email = factory.GetEmail();
+        _password = factory.GetPassword();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var request = new RequestLoginJson()
+        {
+            Email = _email,
+            Password = _password
+        };
+
+        var result = await _httpClient.PostAsJsonAsync(METHOD, request);
+        
+        var body = await result.Content.ReadAsStreamAsync();
+
+        var response = await JsonDocument.ParseAsync(body);
+        
+        response.RootElement.GetProperty("email").GetString().Should().Be(request.Email);
+        response.RootElement.GetProperty("token").GetString().Should().NotBeNullOrWhiteSpace();
+        
+        result.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+    
+    [Theory]
+    [ClassData(typeof(CultureInlineDataTest))]
+    public async Task Error_Empty_Email_Language_Middleware(string language)
+    {
+        var request = new RequestLoginJson()
+        {
+            Email = _email,
+            Password = _password
+        };
+        request.Email = string.Empty;
+
+        _httpClient.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue(language));
+        var result = await _httpClient.PostAsJsonAsync(METHOD, request);
+
+        var body = await result.Content.ReadAsStreamAsync();
+
+        var response = await JsonDocument.ParseAsync(body);
+
+        var errors = response.RootElement.GetProperty("errorMessages").EnumerateArray();
+        var expectedMessage = ResourceErrorMessages.ResourceManager.GetString("USER_EMAIL_REQUIRED", new CultureInfo(language));
+        errors.Should().HaveCount(1).And.Contain(errorMessage => 
+            errorMessage.GetString()!.Equals(expectedMessage));
+        
+        result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+    
+    [Fact]
+    public async Task Error_Not_Matching_Password()
+    {
+        var request = new RequestLoginJson()
+        {
+            Email = _email,
+            Password = "StarWithoutNumber123@"
+        };
+
+        var result = await _httpClient.PostAsJsonAsync(METHOD, request);
+
+        var body = await result.Content.ReadAsStreamAsync();
+
+        var response = await JsonDocument.ParseAsync(body);
+        
+        var errors = response.RootElement.GetProperty("errorMessages").EnumerateArray();
+        errors.Should().HaveCount(1).And.Contain(errorMessage => 
+            errorMessage.GetString()!.Equals(ResourceErrorMessages.LOGIN_EMAIL_OR_PASSWORD_INVALID));
+        
+        result.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+    }
+    
+    [Fact]
+    public async Task Error_User_Not_Found()
+    {
+        var request = new RequestLoginJson()
+        {
+            Email = "teste@test.com.br",
+            Password = _password
+        };
+
+        var result = await _httpClient.PostAsJsonAsync(METHOD, request);
+
+        var body = await result.Content.ReadAsStreamAsync();
+
+        var response = await JsonDocument.ParseAsync(body);
+        
+        var errors = response.RootElement.GetProperty("errorMessages").EnumerateArray();
+        errors.Should().HaveCount(1).And.Contain(errorMessage => 
+            errorMessage.GetString()!.Equals(ResourceErrorMessages.LOGIN_EMAIL_OR_PASSWORD_INVALID));
+        
+        result.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+    }
+}
